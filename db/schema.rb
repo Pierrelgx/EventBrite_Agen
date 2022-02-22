f8163ba9580b6dd145293ff78e089592ec6d727e
Pierrooow <97539842+Pierrooow@users.noreply.github.com>
2022-02-22 09:05:35 +0100

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_132215) do
-
-  create_table "attendances", force: :cascade do |t|
-    t.string "stripe_customer_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.datetime "start_date"
-    t.integer "duration"
-    t.string "title"
-    t.text "description"
-    t.integer "price"
-    t.string "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "encrypted_password"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
